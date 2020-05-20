7c35e68b96bfb3f1737f79aa1ac06d57ac434050
bhagyashreep25 <bagsphadnis@gmail.com>
2020-05-20 20:30:42 +0530

    get individual book page urls from category page urls

--- web_scraper.py
+++ web_scraper.py
@@ -14,8 +14,23 @@ def getcategoriesURLs(url):
 	categories_urls = categories_urls[1:]
 	return categories_urls
 
+def getProductURLs(url):
+	soup = getAndParseURL(url)
+	booksURLs = []
+	booksURLs.extend(getBooksURLs(url, soup))
+	return booksURLs
+
+def getBooksURLs(url, soup):
+	product_urls = ["/".join(url.split("/")[:-4]) + "/" + x.div.a.get('href')[9:] for x in soup.findAll("article", class_ = "product_pod")]
+	return product_urls
+
+
 # main
 url = "http://books.toscrape.com/index.html"
 # soup = getAndParseURL(url)
 categories_list = getcategoriesURLs(url)
-print(categories_list)
+# print(categories_list)
+for category in categories_list:
+	listofbooks = getProductURLs(category)
+	print(str(len(listofbooks)))
+	print(listofbooks)
